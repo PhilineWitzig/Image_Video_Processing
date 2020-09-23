b5a64c8beabbbdc57926bb5bf1502732b37b3c4b
Philine Witzig <philine.witzig@rwth-aachen.de>
2020-09-23 09:38:47 +0200

    Init lab1

---
+++ lab_1_witzig/run_lab_1_witzig.m
@@ -0,0 +1,24 @@
+% Lab 1 - Philine Witzig and 23.09.2020
+
+
+%% Exercise 1 - "Images and Color Tables"
+
+
+
+%% Exercise 2 - "Image Qunatization"
+
+
+
+%% Exercise 3 - "Filtering"
+
+
+%% Exercise 4 - "Correlation"
+
+
+%% Exercise 5 - "Resampling"
+
+
+%% Exercise 6 - "Phase and Magnitude of th 2DFT"
+
+
+%% Exercise 7 - "Weber Law"
