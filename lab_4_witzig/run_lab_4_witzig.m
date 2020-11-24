a21baa161a42cc187a315bd54b114895461a7689
Philine Witzig <philine.witzig@rwth-aachen.de>
2020-11-24 16:29:14 +0100

    preparing lab 4 for upload

--- lab_4_witzig/run_lab_4_witzig.m
+++ lab_4_witzig/run_lab_4_witzig.m
@@ -1,13 +1,9 @@
 % Lab 4 - Philine Witzig 17.11.2020
-
-%pc = pcread("data/");
-%pcshow(pc);
-
+addpath('data/');
+import angularSimilarity.*
 
 subj_data = readtable("data/subjective_results.csv");
 subj_data(1, :) = [];
-% drop model names and sciper number
-%subj_data = subj_data(2:height, 1:width);
 
 exercise = input("Enter the number of the exercise you want to execute: ");
 subj_data_cleaned = detect_outliers(subj_data);
@@ -16,9 +12,6 @@ while (~isempty(setdiff(subj_data_cleaned,subj_data)))
     subj_data = subj_data_cleaned;
     subj_data_cleaned = detect_outliers(subj_data);
 end 
-
-disp(subj_data_cleaned);
-
 
 switch exercise
     case 1
@@ -38,10 +31,12 @@ switch exercise
     else
         myDir = pwd;
         pc_files = dir(fullfile(myDir, 'models')); % get all files
+        % store the objective metric values in a table
         T = cell2table(cell(0, 8));
         T.Properties.VariableNames = {'filename', 'point2point_MSE', 'point2point_HD', 'angular', 'rgb_1', 'rgb_2', 'yuv', 'br'};
         data = load("data/data.mat").data;
         for f_ref=1:length(pc_files)
+            % get reference file
             cur_ref_file_name = pc_files(f_ref).name;
 
             if contains(cur_ref_file_name, "ref")
@@ -49,8 +44,8 @@ switch exercise
                 cur_model = strsplit(cur_ref_file_name, "_");
                 cur_model = cur_model(1);
 
-
                 for f_eval=1:length(pc_files)
+                    % get evaluation file
                     cur_eval_file_name = pc_files(f_eval).name;
 
                     if contains(cur_eval_file_name, cur_model) && ~(contains(cur_eval_file_name, "ref"))
@@ -69,7 +64,7 @@ switch exercise
                         [idcs_er, dist_er] = knnsearch(eval, ref, 'Distance', 'euclidean'); % from  ref to eval
 
                         % If normals don't exist yet, uncomment this code to
-                        % compute normals (this may take a while)
+                        % compute normals (this may take a while, thus we have them precomputed)
                         % normals_ref = pcnormals(pc_ref, 128);
                         % pc_ref.Normal = normals_ref;
                         % normals_eval = pcnormals(pc_eval, 128);
@@ -77,23 +72,28 @@ switch exercise
                         % pcwrite(pc_ref, cur_ref_file_name);
                         % pcwrite(pc_eval, cur_eval_file_name);
 
+                        % apply the different metrics
                         qual_point2point = Point2Point(dist_re, dist_er);
                         qual_plane2plane = Plane2Plane(pc_ref, pc_eval, idcs_re, idcs_er);
                         qual_color = ColorMetric(pc_ref, pc_eval, idcs_re, idcs_er);
                         bitrate = data(strcmp(data.filename, cur_eval_file_name), :).bpp;
+                        
+                        % store results in table
                         T = [T;{cur_eval_file_name, qual_point2point(1), qual_point2point(2), qual_plane2plane, qual_color(1), qual_color(2), qual_color(1,3), bitrate}];
-                        writetable(T, "objective.csv");
+  
                     end
                 end
             end
         end
+        % store table
+        writetable(T, "objective.csv");
     end
-    
+    disp(T);
     plot_objectives(T);
 
     case 3
     %% Exercise 2.5 - "Benchmarking of Objective Quality Metrics"
-    plot_DMOS_objective("dmos.csv", "objective.csv", subj_data_cleaned);
+    plot_DMOS_objective("dmos.csv", "objective.csv");
 
 end
     
@@ -117,7 +117,7 @@ function T=detect_outliers(T)
         MOS_r2_y(content, :) = mean(table2array(T(content:content + 8, 2:participants)));
     end
     
-    % get r1
+    % Init PLCC array
     PLCC_r1 = zeros(participants, 1);
     PLCC_r2 = zeros(participants, 1);
     
@@ -127,12 +127,14 @@ function T=detect_outliers(T)
         denominator_r1 = sqrt(n_r1 * sum(MOS_r1.^2) - sum(MOS_r1)^2) * sqrt(n_r1 * sum(score_subj.^2) - sum(score_subj)^2);
         PLCC_r1(subj) = nominator_r1/denominator_r1;
         
+        % compute MOS
         MOS_subj = MOS_r2_y(:, subj-1);
         nominator_r2 = n_r2 * sum(MOS_r2_x .* MOS_subj) - sum(MOS_r2_x) * sum(MOS_subj);
         denominator_r2 = sqrt(n_r2 * sum(MOS_r2_x.^2) - sum(MOS_r2_x)^2) * sqrt(n_r2 * sum(MOS_subj.^2) - sum(MOS_subj)^2);
         PLCC_r2(subj) = nominator_r2/denominator_r2;
     end
     
+    % Remove the worst outlier
     worst_value = 0.0;
     worst_candidate = 0;
     for i=2:participants
@@ -152,6 +154,7 @@ function T=detect_outliers(T)
 end
 
 function [DMOS,DV]=get_DMOS(T)
+    % Compute DMOS and DV
     [stimuli, participants] = size(T);
     DMOS = zeros(stimuli, 1); 
     DV = zeros(stimuli, participants - 1);
@@ -171,15 +174,17 @@ function [DMOS,DV]=get_DMOS(T)
 end
 
 function CI=get_CI(DV)
+    % Compute the confidence intervals, alpha=0.05
     [stimuli, participants] = size(DV);
     CI = zeros(stimuli, 1); % value of reference value is not included
     for j=1:stimuli
-        CI(j, :) = icdf('T', 1 - (0.95/2), participants - 1) .* (std(DV(j, :)) / sqrt(participants));
+        CI(j, :) = icdf('T', 1 - (.05/2), participants - 1) .* (std(DV(j, :)) / sqrt(participants));
     end
 end
 
 function plot_DMOS(DMOS, CI)
-   
+    % Produce the DMOS vs bitrate plots
+    
     [stimuli, participants] = size(DMOS);
     
     % remove reference from DMOS and CI
@@ -206,18 +211,21 @@ function plot_DMOS(DMOS, CI)
         else
             figure('name', "Rhetorician");
         end
-        errorbar(x_codec1, codec1, CI(i:i+3));
+        % swap ordering here to make color coding consistent with other
+        % tasks
+        errorbar(x_codec2, codec2, CI(i+4:i+7));
         hold on;
-        errorbar(x_codec2, codec2, CI(i+4:i+7), 'r');
+        errorbar(x_codec1, codec1, CI(i:i+3), 'r');
         ylim([0 6]);
         yticks([1 2 3 4 5]);
         xlabel("Bitrates");
         ylabel("DMOS");
-        legend({"Codec 1: pcc geo color", "Codec 2: cwi pcl"}, 'location', 'northwest');
+        legend({"Codec 1: cwi pcl", "Codec 2: pcc geo color"}, 'location', 'northwest');
     end
 end
 
 function qual = Point2Point(dist_re, dist_er)
+    % Get the point 2 point metric values using MSE and Hausdorff
     qual_MSE = max(sum(dist_re .^2) / length(dist_re), sum(dist_er .^2) / length(dist_er));
        
     hausdorff_er = max(min(dist_re,[],2)); % Directed from eval to ref
@@ -228,31 +236,15 @@ function qual = Point2Point(dist_re, dist_er)
     
 end
 
-function qual = Plane2Plane(pc_ref, pc_eval, icds_re, idcs_er)
-    
-    normals_ref = pc_ref.Normal;
-    normals_eval = pc_eval.Normal;
+function qual = Plane2Plane(pc_ref, pc_eval)
+    % Get the plane to plane metric value using angular similarity
+    qual = angularSimilarity(pc_ref, pc_eval, 'MSE');
 
-    % from eval to ref
-    normals_ref_matched = normals_ref(icds_re, :);
-    theta_hat = acos(dot(normals_ref_matched, normals_eval, 2) ./ (norm(normals_ref_matched) .* norm(normals_eval)));
-    theta = min(theta_hat, pi - theta_hat);
-    qual_re = 1 - (2 .* theta ./ pi);
-    qual_re_MSE = sum(qual_re .^2) / length(qual_re);
-    
-    % from ref to eval
-    normals_eval_matched = normals_eval(idcs_er, :);
-    theta_hat = acos(dot(normals_eval_matched, normals_ref, 2) ./ (norm(normals_eval_matched) .* norm(normals_ref)));
-    theta = min(theta_hat, pi - theta_hat);
-    qual_er = 1 - (2 .* theta ./ pi);
-    qual_er_MSE = sum(qual_er .^2) / length(qual_er);
-    
-    % get max
-    qual = max(qual_re_MSE, qual_er_MSE);
 end
 
 function qual = ColorMetric(pc_ref, pc_eval, idcs_re, idcs_er)
-
+    % Computer the color metric values on RGB and YUV space
+    % Note that there are two version for computing the PSNR on RGB
     
     colors_ref_matched = double(pc_ref.Color(idcs_re, :));
     colors_eval_matched = double(pc_eval.Color(idcs_er, :));
@@ -281,6 +273,7 @@ function qual = ColorMetric(pc_ref, pc_eval, idcs_re, idcs_er)
 end
 
 function colors_YUV = RGB2YUV(colors_RGB)
+    % Get the YUV values from RGB
     colors_YUV = zeros(size(colors_RGB));
     colors_YUV(:, 1) = 0.299 .* colors_RGB(:, 1) + 0.587 .* colors_RGB(:, 2) + 0.144 .* colors_RGB(:, 3);
     colors_YUV(:, 2) = 0.493 .* (colors_RGB(:, 3) - colors_YUV(:, 1));
@@ -288,18 +281,28 @@ function colors_YUV = RGB2YUV(colors_RGB)
 end
 
 function PSNR = get_PSNR(colors)
-
+    % Compute the PSNR according to the formula in the assignment
     [height, width] = size(colors);
     MSE = sum(colors.^2, 'all') / (height * width);
     PSNR = 10 * log10(255^2 / MSE);
 end
 
 function plot_objectives(T)
+    % Produce plots for objective metric values
+    
     data = table2array(T(:, 2:8));
     [row, column] = size(T);
     for metric=1:6
         for i=1:8:row
-            figure;
+            if i == 1
+                figure('name', "Guanyin");
+            elseif i == 9
+                figure('name', "Longdress");
+            elseif i == 17
+                figure('name', "Phil");   
+            else
+                figure('name', "Rhetorician");
+            end
             codec1 = data(i:i+3,metric);
             br_1 = data(i:i+3, 7);
             codec2 = data(i+4:i+7, metric);
@@ -310,44 +313,49 @@ function plot_objectives(T)
             ylabel("Objective Metric");
             hold on;
             plot(br_2, codec2, 'r');
-            legend({"Codec 1: cwi pcl", "Codec 2: pcc geo color"}, 'location', 'northeast');
+            legend({"Codec 1: cwi pcl", "Codec 2: pcc geo color"}, 'location', 'northeastoutside');
         end
     end
 end
 
-function plot_DMOS_objective(dmos_file, obj_file, subj_data)
+function plot_DMOS_objective(dmos_file, obj_file)
+    % Produce plots for DMOS vs objective metric values
+    % Fit the objective values
+    
     T_DMOS = readtable(dmos_file);
     T_OBJ = readtable(obj_file);
-    subj_data.Properties.VariableNames{'Var1'} = 'filename';
-    [models, participants] = size(subj_data);
     T = join(T_OBJ, T_DMOS, 'Keys','filename');
-    T_subj_obj = join(T_OBJ, subj_data, 'Keys', 'filename');
     [models, metrics] = size(T_OBJ);
     
     T_corr = cell2table(cell(0, 10));
     T_corr.Properties.VariableNames = {'metric', 'pearson', 'spearman', 'rmse', 'pearson_fit_linear', 'spearman_fit_linear', 'rmse_fit_linear', 'pearson_fit_cubic', 'spearman_fit_cubic', 'rmse_fit_cubic'};
    
     for metric=2:metrics-1
-        obj_scores = table2array(T_subj_obj(:,metric)); % metric values
+        obj_scores = table2array(T(:, metric)); % metric values
+        subj_scores = T.dmos;
+        
+        % produce plot
         figure;
-        e = errorbar(obj_scores, T.dmos, T.ci);
+        e = errorbar(obj_scores, subj_scores, T.ci);
         e.LineStyle = 'none';
         title(T.Properties.VariableNames{metric});
         xlabel("Objective metric");
         ylabel("DMOS");
         hold on;
-       
-        subj_scores = mean(table2array(T_subj_obj(:, metrics+1:end)), 2); % MOS 
-        pearson = corr(obj_scores, subj_scores, 'Type', 'Pearson');
-        spearman = corr(obj_scores, subj_scores, 'Type', 'Spearman');
+      
+        % get correlation coeefficients and RMSEs
+        pearson = corr(subj_scores, obj_scores, 'Type', 'Pearson');
+        spearman = corr(subj_scores, obj_scores, 'Type', 'Spearman');
         rmse = sqrt(sum((obj_scores - subj_scores).^2, 'all'));
-    
+        
+        % fit data 
         linear_fit = polyfit(obj_scores, subj_scores, 1);
         cubic_fit = polyfit(obj_scores, subj_scores, 3);
         
         obj_scores_linear = polyval(linear_fit, obj_scores);
         obj_scores_cubic = polyval(cubic_fit, obj_scores);
         
+        % reproduce plots on fitted data
         figure;
         e = errorbar(obj_scores_linear, T.dmos, T.ci);
         e.LineStyle = 'none';
@@ -364,14 +372,16 @@ function plot_DMOS_objective(dmos_file, obj_file, subj_data)
         ylabel("DMOS");
         hold on;
         
-        pearson_linear = corr(obj_scores_linear, subj_scores, 'Type', 'Pearson');
-        spearman_linear = corr(obj_scores_linear, subj_scores, 'Type', 'Spearman');
+        % recompute coefficients on fitted data
+        pearson_linear = corr(subj_scores, obj_scores_linear, 'Type', 'Pearson');
+        spearman_linear = corr(subj_scores, obj_scores_linear, 'Type', 'Spearman');
         rmse_linear = sqrt(sum((obj_scores_linear - subj_scores).^2, 'all'));
         
-        pearson_cubic = corr(obj_scores_cubic, subj_scores, 'Type', 'Pearson');
-        spearman_cubic = corr(obj_scores_cubic, subj_scores, 'Type', 'Spearman');
+        pearson_cubic = corr(subj_scores, obj_scores_cubic, 'Type', 'Pearson');
+        spearman_cubic = corr(subj_scores, obj_scores_cubic, 'Type', 'Spearman');
         rmse_cubic = sqrt(sum((obj_scores_cubic - subj_scores).^2, 'all'));
         
+        % store results in table
         T_corr = [T_corr; {T.Properties.VariableNames{metric}, pearson, spearman, rmse, pearson_linear, spearman_linear, rmse_linear, pearson_cubic, spearman_cubic, rmse_cubic}];
         
     end
